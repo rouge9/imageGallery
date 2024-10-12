defb52cda41fb08953e0a064debee9e35c170980
rouge9 <robelged1992@gmail.com>
2024-10-12 07:48:38 -0900

    added font family

--- src/components/Shared/Header.tsx
+++ src/components/Shared/Header.tsx
@@ -1,9 +1,6 @@
 import { Button } from "@/components/ui/button";
-import { useState } from "react";
-// import Logo from "./Logo";
 
 export default function Header() {
-  const [isHover, setIsHover] = useState(false);
   return (
     <header className=" py-4 relative">
       {/* desktop */}
@@ -31,28 +28,7 @@ export default function Header() {
         >
           <a href="/login">Login</a>
         </Button>
-        <div
-          className="cursor-pointer hover:scale-105 transition-transform"
-          onClick={() => setIsHover(!isHover)}
-        >
-          {isHover ? (
-            <img
-              src="/assets/img/logoColors.svg"
-              alt="Jive Logo"
-              width={80}
-              height={30}
-              className="h-8 md:h-10"
-            />
-          ) : (
-            <img
-              src="/assets/img/logoFull.svg"
-              alt="Jive Logo"
-              width={80}
-              height={30}
-              className="h-8 md:h-10"
-            />
-          )}
-        </div>
+        <div />
         <Button className="rounded-full px-4 py-2 font-semibold text-sm  hover:bg-sky-600">
           <a href="/signup">Sign up</a>
         </Button>
